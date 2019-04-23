122248a1bdd93be426bed5af48fb4042ece91b01
Amy M Haddad <amy.m.haddad@gmail.com>
2019-04-23 14:37:45 -0400

    final changes to word_finder

--- ex_46/word_finder.py
+++ ex_46/word_finder.py
@@ -1,22 +1,29 @@
+"""Create a program that reads in a file and counts the frequency of words in the file. Then construct a histogram displaying the words and the frequency"""
 
-filename = 'text.py'
 import csv
+FILENAME = 'text.py'
 
-def word_and_freq(filename):
+def word_and_freq(FILENAME):
+
+    """Read in text from a file; add each word and its frequency to dictionary"""
+
     counter = {}
-    with open(filename, newline='') as csvfile:
+    with open(FILENAME, newline='') as csvfile:
         linereader = csv.reader(csvfile, delimiter=' ')
         for row in linereader:
             for word in row:
                 counter[word] = counter.get(word, 0) + 1
     return counter
 
-word_counter = word_and_freq(filename)
+word_counter = word_and_freq(FILENAME)
 
 def show_freq(word_counter):
+
+    """Create historgram dislaying the words and their frequncy from the dictionary"""
+
     template = ''
     for word, freq in word_counter.items():
         template += word + ': ' + '*' * freq + ' \n'
-    return template.strip()            
+    return template.strip()
 
 print(show_freq(word_counter))
