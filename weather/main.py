42172c6ef79b4b658f8225195d8d6d2caf88f4ae
Amy M Haddad <amy.m.haddad@gmail.com>
2019-07-02 11:12:26 -0400

    Final changes for PR

--- weather/main.py
+++ weather/main.py
@@ -2,15 +2,15 @@
 
 from weather_management import find_weather_for_user_city
 
+
 def get_local_weather():
     """Return a city's current weather based on user's input"""
 
     temp_kelvin = find_weather_for_user_city()
-
     fahrenheit_temp = f"{(temp_kelvin['main']['temp'] * 9/5) - 459.67:.0f}"
+
     print(fahrenheit_temp)
+
 
 if __name__ == "__main__":
     get_local_weather()
-
-
--- weather/weather_management.py
+++ weather/weather_management.py
@@ -1,14 +1,13 @@
 """Try to make API call based on user input"""
 
-from user_input import get_user_city
 import requests
-import json
+from user_input import get_user_city
 
 url = "http://api.openweathermap.org/data/2.5/weather"
 params = {"APPID": "31422cb605846e0dcdd4f75cbe27cd38"}
 headers = {"Content-Type": "application/json"}
 
-def add_user_city_to_request_parameter(params):
+def _add_user_city_to_request_parameter(params):
     """Add user's city to the request parameter"""
     user_city = get_user_city()
     params.setdefault("q", user_city.title())
@@ -18,10 +17,10 @@ def add_user_city_to_request_parameter(params):
 def find_weather_for_user_city():
     """Get the temperature for a city in Kelvin. Raise an error is city doesn't exist."""
     try:
-        params_with_city = add_user_city_to_request_parameter(params)
+        params_with_city = _add_user_city_to_request_parameter(params)
         response = requests.get(url, params_with_city)
         json_response = response.json()
-        
+
     except KeyError as kerr:
         print(f"A {kerr} error has occurred.")
 
