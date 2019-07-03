558556094be026a81c07d9fa68aff0586237d08b
Amy M Haddad <amy.m.haddad@gmail.com>
2019-07-03 07:35:36 -0400

    Final changes from live PR review

--- weather/main.py
+++ weather/main.py
@@ -1,16 +1,20 @@
 """A program that finds the current weather for any city in the world in Fahrenheit."""
 
-from weather_management import find_weather_for_user_city
+from weather_management import find_weather_for_user_city, convert_kelvin_to_fahrenheit
+from user_input import get_user_city
 
 
-def get_local_weather():
+def main():
     """Return a city's current weather based on user's input"""
+    #input
+    city = get_user_city()
 
-    temp_kelvin = find_weather_for_user_city()
-    fahrenheit_temp = f"{(temp_kelvin['main']['temp'] * 9/5) - 459.67:.0f}"
+    #logic
+    kelvin_temperature = find_weather_for_user_city(city)
+    fahrenheit_temperature = convert_kelvin_to_fahrenheit(kelvin_temperature)
 
-    print(fahrenheit_temp)
-
+    #printing
+    print(fahrenheit_temperature)
 
 if __name__ == "__main__":
-    get_local_weather()
+    main()
--- weather/weather_management.py
+++ weather/weather_management.py
@@ -1,29 +1,26 @@
 """Try to make API call based on user input"""
 
 import requests
-from user_input import get_user_city
 
-PARAMS = {"APPID": "31422cb605846e0dcdd4f75cbe27cd38"}
-HEADERS = {"Content-Type": "application/json"}
+APPID = "31422cb605846e0dcdd4f75cbe27cd38"
 
-
-def add_user_city_to_request_parameter():
-    """Add user's city to the request parameter"""
-    global PARAMS
-    user_city = get_user_city()
-    PARAMS.setdefault("q", user_city.title())
-    return PARAMS
-
-
-def find_weather_for_user_city():
+def find_weather_for_user_city(city):
     """Get the temperature for a city in Kelvin. Raise an error is city doesn't exist."""
 
+    params = {
+        'APPID': APPID,
+        'q': city,
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+        }
+
     try:
-        params_with_city = add_user_city_to_request_parameter()
         response = requests.get(
             "http://api.openweathermap.org/data/2.5/weather",
-            params=params_with_city,
-            headers=HEADERS,
+            params=params,
+            headers=headers,
         )
         json_response = response.json()
 
@@ -35,3 +32,7 @@ def find_weather_for_user_city():
 
     else:
         return json_response
+
+def convert_kelvin_to_fahrenheit(kelvin_temperature):
+    """Convert the weather from Kelvin to Fahrenheit"""
+    return f"{(kelvin_temperature['main']['temp'] * 9/5) - 459.67:.0f}"
